eec2b0e89d309c90c89c663b611e5eb83033ef86
刘方靖 <liufangjing@shifang.co>
2023-11-09 16:08:59 +0800

    finish

--- case_data/user/user_data.py
+++ case_data/user/user_data.py
@@ -1,6 +1,15 @@
-
+from utils.encrypt import encrpt
 
 
 class UserData:
     def login_data(self):
+        """
+        login在base里完善了不用单独写data，此为展示
+        :return:
+        """
+        # 读取variables文件对应的接口请求架构
         variables_temp = self.get_variables(module_name="log", variables_name="role_list_filter")
+        # 补全架构确实的数据
+        args = [("passwd", ), ("login", ), ("random", )]
+        variables = self.modify_variables(target_json=variables_temp, args=args)
+        return variables
